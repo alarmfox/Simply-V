5d4630dec6c21c54e437654d0619635551ede210
Giuseppe Capasso <capassog97@gmail.com>
2025-11-01 14:51:59 +0100

    linting: applications

--- sw/SoC/projects/freertos/app/basic-two-tasks/main.c
+++ sw/SoC/projects/freertos/app/basic-two-tasks/main.c
@@ -5,12 +5,10 @@
 #include "uninasoc.h"
 
 #define SOURCES_NUM 3
-static xlnx_tim_t timer = {
-    .base_addr = TIM0_BASEADDR,
-    .counter = 20000000,
-    .reload_mode = TIM_RELOAD_AUTO,
-    .count_direction = TIM_COUNT_DOWN
-};
+static xlnx_tim_t timer = {.base_addr = TIM0_BASEADDR,
+                           .counter = 20000000,
+                           .reload_mode = TIM_RELOAD_AUTO,
+                           .count_direction = TIM_COUNT_DOWN};
 #endif // USE_UNINASOC
 
 #define TASK1_PRIORITY (tskIDLE_PRIORITY + 1)
@@ -19,119 +17,113 @@ static xlnx_tim_t timer = {
 #define TASK1_PARAMETER (1)
 #define TASK2_PARAMETER (2)
 
-
 static void Task1(void *pvParameters) {
 
-    uint32_t a;
-    uint32_t b = 3;
+  uint32_t a;
+  uint32_t b = 3;
 
-    configASSERT( ( ( uint32_t ) pvParameters ) == TASK1_PARAMETER );
-    size_t free_heap = xPortGetFreeHeapSize();
-    configASSERT( free_heap > 0 );
+  configASSERT(((uint32_t)pvParameters) == TASK1_PARAMETER);
+  size_t free_heap = xPortGetFreeHeapSize();
+  configASSERT(free_heap > 0);
 
-    while (1) {
-    	free_heap = xPortGetFreeHeapSize();
-        configASSERT( free_heap > 0 );
-    	a = b + (uint32_t) pvParameters;
-        (void) a;
-        taskYIELD();
-    }
+  while (1) {
+    free_heap = xPortGetFreeHeapSize();
+    configASSERT(free_heap > 0);
+    a = b + (uint32_t)pvParameters;
+    (void)a;
+    taskYIELD();
+  }
 }
 
 static void Task2(void *pvParameters) {
 
-    uint32_t a;
-    uint32_t b = 3;
-    
-    configASSERT( ( ( uint32_t ) pvParameters ) == TASK2_PARAMETER );
-    size_t free_heap = xPortGetFreeHeapSize();
-    configASSERT( free_heap > 0 );
+  uint32_t a;
+  uint32_t b = 3;
 
-    while (1) {
-    	    free_heap = xPortGetFreeHeapSize();
-            configASSERT( free_heap > 0 );
+  configASSERT(((uint32_t)pvParameters) == TASK2_PARAMETER);
+  size_t free_heap = xPortGetFreeHeapSize();
+  configASSERT(free_heap > 0);
 
-	    a = b - (uint32_t) pvParameters;
-            (void)a;
-	    taskYIELD();
-   } 
+  while (1) {
+    free_heap = xPortGetFreeHeapSize();
+    configASSERT(free_heap > 0);
+
+    a = b - (uint32_t)pvParameters;
+    (void)a;
+    taskYIELD();
+  }
 }
 
 void vAssertCalled(const char *file, int line) {
 
-     const char *f=file;
-     int l=line;
-     (void) f;
-     (void) l;
-     __asm("ebreak");
+  const char *f = file;
+  int l = line;
+  (void)f;
+  (void)l;
+  __asm("ebreak");
 }
 
-void vPortSetupTimerInterrupt(void){
+void vPortSetupTimerInterrupt(void) {
 
 #ifdef USE_UNINASOC
-    int ret;
-    uint32_t priorities[SOURCES_NUM] = { 1, 1, 1 };
-    plic_init();
-    plic_configure(priorities, SOURCES_NUM);
-    plic_enable_all();
+  int ret;
+  uint32_t priorities[SOURCES_NUM] = {1, 1, 1};
+  plic_init();
+  plic_configure(priorities, SOURCES_NUM);
+  plic_enable_all();
 
-    ret = xlnx_tim_init(&timer);
-    if(ret != UNINASOC_OK) {
-	printf("Cannot init TIMER\n\r");
-	return;
-    }
- 
-    ret = xlnx_tim_configure(&timer);
-    if(ret != UNINASOC_OK) {
-	printf("Cannot configure TIMER\n\r");
-	return;
-    }
+  ret = xlnx_tim_init(&timer);
+  if (ret != UNINASOC_OK) {
+    printf("Cannot init TIMER\n\r");
+    return;
+  }
 
-    ret = xlnx_tim_enable_int(&timer);
-    if (ret != UNINASOC_OK) {
-        printf("Cannot enable timer interrupt\r\n");
-	return;
-    }
+  ret = xlnx_tim_configure(&timer);
+  if (ret != UNINASOC_OK) {
+    printf("Cannot configure TIMER\n\r");
+    return;
+  }
 
-    ret = xlnx_tim_start(&timer);
-    if (ret != UNINASOC_OK) {
-        printf("Cannot start timer\r\n");
-	return;
-    }
+  ret = xlnx_tim_enable_int(&timer);
+  if (ret != UNINASOC_OK) {
+    printf("Cannot enable timer interrupt\r\n");
+    return;
+  }
+
+  ret = xlnx_tim_start(&timer);
+  if (ret != UNINASOC_OK) {
+    printf("Cannot start timer\r\n");
+    return;
+  }
 #endif // USE_UNINASOC
 }
 
-int main(){
+int main() {
 
 #ifdef USE_UNINASOC
-    uninasoc_init();
-    printf("SIMPLY-V FreeRTOS DEMO!\n\r");
+  uninasoc_init();
+  printf("SIMPLY-V FreeRTOS DEMO!\n\r");
 #endif // USE_UNINASOC
 
+  // Create FreeRTOS Task
+  BaseType_t mem_1 = xTaskCreate(Task1, "t1", configMINIMAL_STACK_SIZE,
+                                 (void *)TASK1_PARAMETER, TASK1_PRIORITY, NULL);
 
-    // Create FreeRTOS Task
-    BaseType_t mem_1=xTaskCreate(Task1,"t1", configMINIMAL_STACK_SIZE,
-			(void *)TASK1_PARAMETER,
-			TASK1_PRIORITY,
-			NULL);
+  BaseType_t mem_2 = xTaskCreate(Task2, "t2", configMINIMAL_STACK_SIZE,
+                                 (void *)TASK2_PARAMETER, TASK2_PRIORITY, NULL);
 
-    BaseType_t mem_2=xTaskCreate(Task2,"t2", configMINIMAL_STACK_SIZE,
-			(void *)TASK2_PARAMETER, TASK2_PRIORITY,
-			NULL);
+  configASSERT(mem_1 == pdPASS);
 
-    configASSERT( mem_1 == pdPASS );
-  
-    configASSERT( mem_2 == pdPASS );
- 
-    size_t free_heap = xPortGetFreeHeapSize();
- 
-    configASSERT( free_heap > 0 );
-    vTaskStartScheduler();
+  configASSERT(mem_2 == pdPASS);
 
-    configASSERT(0); //insufficient RAM->scheduler task returns->vAssertCalled() called
+  size_t free_heap = xPortGetFreeHeapSize();
 
-    while(1);
+  configASSERT(free_heap > 0);
+  vTaskStartScheduler();
 
-    return 0;
+  configASSERT(0); // insufficient RAM->scheduler task returns->vAssertCalled() called
+
+  while (1);
+
+  return 0;
 }
-
--- sw/SoC/projects/freertos/app/simple-queue/main.c
+++ sw/SoC/projects/freertos/app/simple-queue/main.c
@@ -1,4 +1,3 @@
-
 #include "FreeRTOS.h"
 #include "queue.h"
 #include "task.h"
@@ -14,88 +13,87 @@
 static QueueHandle_t xQueue = NULL;
 
 static void prvQueueMyTask1(void *pvParameters) {
-    const unsigned long ulValueToSend = 100UL;
+  const unsigned long ulValueToSend = 100UL;
 
-    configASSERT(((unsigned long)pvParameters) == mainQUEUE_SEND_PARAMETER);
- 
-    size_t free_heap=0;
-    free_heap=xPortGetFreeHeapSize();
-    configASSERT( free_heap > 0 );
+  configASSERT(((unsigned long)pvParameters) == mainQUEUE_SEND_PARAMETER);
 
-    while (1) {
- 
-        free_heap=xPortGetFreeHeapSize();
-        configASSERT( free_heap > 0 );
- 
-        xQueueSend(xQueue, &ulValueToSend, 0U);
-        taskYIELD();
-    }
+  size_t free_heap = 0;
+  free_heap = xPortGetFreeHeapSize();
+  configASSERT(free_heap > 0);
+
+  while (1) {
+
+    free_heap = xPortGetFreeHeapSize();
+    configASSERT(free_heap > 0);
+
+    xQueueSend(xQueue, &ulValueToSend, 0U);
+    taskYIELD();
+  }
 }
 
 static void prvQueueMyTask2(void *pvParameters) {
-    (void) pvParameters;
-    while (1) {
-        unsigned long ulReceivedValue;
-        xQueueReceive(xQueue, &ulReceivedValue, portMAX_DELAY);
- 
-        size_t free_heap=0;
-        free_heap=xPortGetFreeHeapSize();
-        configASSERT( free_heap > 0 );
+  (void)pvParameters;
+  while (1) {
+    unsigned long ulReceivedValue;
+    xQueueReceive(xQueue, &ulReceivedValue, portMAX_DELAY);
 
-        if (ulReceivedValue == 100UL) {
-            ulReceivedValue = 0U;
-        }
-        configASSERT(ulReceivedValue == 0U);
-        taskYIELD();
+    size_t free_heap = 0;
+    free_heap = xPortGetFreeHeapSize();
+    configASSERT(free_heap > 0);
+
+    if (ulReceivedValue == 100UL) {
+      ulReceivedValue = 0U;
     }
+    configASSERT(ulReceivedValue == 0U);
+    taskYIELD();
+  }
 }
 
 void vAssertCalled(const char *file, int line) {
 
-     const char *f=file;
-     int l=line;
-     (void) f;
-     (void) l;
-     __asm("ebreak");
+  const char *f = file;
+  int l = line;
+  (void)f;
+  (void)l;
+  __asm("ebreak");
 }
 
-void vPortSetupTimerInterrupt( void ){
+void vPortSetupTimerInterrupt(void) {
 
-//define if you need to set the timer interrupt,otherwise an empty definition is still necessary to overwrite the weak definition in port.c and to avoid
-//unwanted jumps to reset handler
-
+  // define if you need to set the timer interrupt,otherwise an empty definition
+  // is still necessary to overwrite the weak definition in port.c and to avoid
+  // unwanted jumps to reset handler
 }
 
-int main(){
+int main() {
 
-    /* Insert your code here */
-    
-    xQueue = xQueueCreate(mainQUEUE_LENGTH, sizeof(unsigned long));
-    configASSERT(xQueue != NULL);
+  /* Insert your code here */
 
-    BaseType_t mem_1=xTaskCreate(prvQueueMyTask2,
-			"MyTask2", configMINIMAL_STACK_SIZE,
-			(void *) mainQUEUE_RECEIVE_PARAMETER,
-			mainQUEUE_RECEIVE_TASK_PRIORITY,
-			NULL);
+  xQueue = xQueueCreate(mainQUEUE_LENGTH, sizeof(unsigned long));
+  configASSERT(xQueue != NULL);
 
-    BaseType_t mem_2=xTaskCreate(prvQueueMyTask1, "MyTask1", configMINIMAL_STACK_SIZE,
-			(void *)mainQUEUE_SEND_PARAMETER, mainQUEUE_SEND_TASK_PRIORITY,
-			NULL);
-			   
-    configASSERT( mem_1 == pdPASS );
-    
-    configASSERT( mem_2 == pdPASS );
-    
-    size_t free_heap=xPortGetFreeHeapSize();
-    
-    configASSERT( free_heap > 0 );
-			    
-    vTaskStartScheduler();
+  BaseType_t mem_1 =
+      xTaskCreate(prvQueueMyTask2, "MyTask2", configMINIMAL_STACK_SIZE,
+                  (void *)mainQUEUE_RECEIVE_PARAMETER,
+                  mainQUEUE_RECEIVE_TASK_PRIORITY, NULL);
 
-    configASSERT(0);  //insufficient RAM->scheduler task returns->vAssertCalled() called
-    
-    while(1);
+  BaseType_t mem_2 = xTaskCreate(
+      prvQueueMyTask1, "MyTask1", configMINIMAL_STACK_SIZE,
+      (void *)mainQUEUE_SEND_PARAMETER, mainQUEUE_SEND_TASK_PRIORITY, NULL);
 
-    return 0;
+  configASSERT(mem_1 == pdPASS);
+
+  configASSERT(mem_2 == pdPASS);
+
+  size_t free_heap = xPortGetFreeHeapSize();
+
+  configASSERT(free_heap > 0);
+
+  vTaskStartScheduler();
+
+  configASSERT(0); // insufficient RAM->scheduler task returns->vAssertCalled() called
+
+  while (1);
+
+  return 0;
 }
