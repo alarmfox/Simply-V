53892da7f12945a8d7c0b51e82e4fd62c4ea69ce
giuseppe <capassog97@gmail.com>
2025-11-03 11:45:31 +0100

    fix: adjusted stack and heap size

--- sw/SoC/projects/freertos/FreeRTOSConfig.h
+++ sw/SoC/projects/freertos/FreeRTOSConfig.h
@@ -52,8 +52,8 @@
 #define configCPU_CLOCK_HZ				( 10000000 )
 #define configTICK_RATE_HZ				( ( TickType_t ) 1 )
 #define configMAX_PRIORITIES			( 7 )
-#define configMINIMAL_STACK_SIZE		( ( unsigned short ) 256 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) 4096 )
+#define configMINIMAL_STACK_SIZE		( ( unsigned short ) 128 )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) 3072 )
 #define configMAX_TASK_NAME_LEN			( 16 )
 #define configUSE_TRACE_FACILITY		0
 #define configUSE_16_BIT_TICKS			0
