13f5ffd77d675758f1886c16d176f6d5684689ce
giuseppe <capassog97@gmail.com>
2025-11-05 16:40:13 +0100

    feat: working timer implementation

--- sw/SoC/projects/freertos/app/simple-timer/main.c
+++ sw/SoC/projects/freertos/app/simple-timer/main.c
@@ -4,67 +4,110 @@
 
 static xlnx_tim_t timer = {
   .base_addr = TIM0_BASEADDR,
-  .counter = 20000000,
+  .counter = 200000,
   .reload_mode = TIM_RELOAD_AUTO,
   .count_direction = TIM_COUNT_DOWN
 };
 
-#define TASK1_PRIORITY (tskIDLE_PRIORITY + 1)
+#define TASK_PRIORITY (tskIDLE_PRIORITY + 1)
 
-#define TASK1_PARAMETER (1)
+#define TASK_PARAMETER (1)
 
 static void Task(void *pvParameters) {
 
-  uint32_t a;
-  uint32_t b = 3;
+  configASSERT(((uint32_t)pvParameters) == TASK_PARAMETER);
 
-  configASSERT(((uint32_t)pvParameters) == TASK1_PARAMETER);
-  size_t free_heap;
+  TickType_t xLastWakeTime;
+  uint32_t amt = (uint32_t) pvParameters;
+  uint32_t counter = 0;
+  const TickType_t xFrequency = 10;
+
+  // Initialise the xLastWakeTime variable with the current time.
+  xLastWakeTime = xTaskGetTickCount();
 
   while (1) {
-    free_heap = xPortGetFreeHeapSize();
-    configASSERT(free_heap > 0);
-
-    a = b + (uint32_t)pvParameters;
-    (void)a;
-
-    printf("Hello from Task\n\r");
+    counter += amt;
+    printf("Task counter: %d\n\r", counter);
+    // Wait for the next cycle.
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
   }
 }
 
-void vExternalTimerTickHandler(void) {
+// https://rcc.freertos.org/Documentation/02-Kernel/05-RTOS-implementation-tutorial/02-Building-blocks/03-The-RTOS-tick
+void vExternalTickIncrement() {
+  BaseType_t xSwitchRequired;
+
+  // Clear interrupt flag
   xlnx_tim_clear_int(&timer);
-  xTaskIncrementTick();
+
+  // Increment RTOS tick count
+  xSwitchRequired = xTaskIncrementTick();
+
+  // If a task was unblocked, yield to it
+  if (xSwitchRequired != pdFALSE) {
+    portYIELD_FROM_ISR(xSwitchRequired);
+  }
 }
 
-void freertos_risc_v_application_interrupt_handler(void) {
+void freertos_risc_v_application_interrupt_handler(uint32_t mcause) {
+  (void) mcause;
 
-    uint32_t interrupt_id = plic_claim();
-    switch (interrupt_id) {
+  uint32_t interrupt_id = plic_claim();
+
+  switch (interrupt_id) {
     case 0x2:
-        printf("Handiling TIM0 interrupt!\r\n");
-        vExternalTimerTickHandler();
+      vExternalTickIncrement();
       break;
     default:
       break;
-    }
-    plic_complete(interrupt_id);
+  }
+  plic_complete(interrupt_id);
 }
 
 void vAssertCalled(const char *file, int line) {
+  (void)file;
+  (void)line;
 
-  const char *f = file;
-  int l = line;
-  (void)f;
-  (void)l;
   __asm("ebreak");
 }
+
+#ifdef configCHECK_FOR_STACK_OVERFLOW
+/* Called if a task overflows its stack space */
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
+  (void)xTask;
+  (void)pcTaskName;
+
+  __asm volatile ("ebreak");
+}
+#endif // configCHECK_FOR_STACK_OVERFLOW
+
+
+#ifdef configUSE_MALLOC_FAILED_HOOK
+/* Called if pvPortMalloc() fails (heap exhausted) */
+void vApplicationMallocFailedHook(void) {
+    __asm volatile ("ebreak");
+}
+#endif // configUSE_MALLOC_FAILED_HOOK
 
 // define if you need to set the timer interrupt,otherwise an empty definition
 // is still necessary to overwrite the weak definition in port.c and to avoid
 // unwanted jumps to reset handler
 void vPortSetupTimerInterrupt(void) {
   int ret;
+  uint32_t priorities[3] = { 1, 1, 1 };
+  
+  plic_init();
+  plic_configure(priorities, 3);
+  plic_enable_all();
+
+  xlnx_tim_init(&timer);
+ 
+  ret = xlnx_tim_configure(&timer);
+  if (ret != UNINASOC_OK) {
+    printf("Cannot configure timer\r\n");
+    return;
+  }
+
   ret = xlnx_tim_enable_int(&timer);
   if (ret != UNINASOC_OK) {
     printf("Cannot enable timer\r\n");
@@ -84,30 +127,13 @@ void vPortSetupTimerInterrupt(void) {
 
 int main() {
 
-  int ret;
-  uint32_t priorities[3] = { 1, 1, 1 };
-
   uninasoc_init();
 
-  plic_init();
-  plic_configure(priorities, 3);
-  plic_enable_all();
- 
-  xlnx_tim_init(&timer);
- 
-  ret = xlnx_tim_configure(&timer);
-  if (ret != UNINASOC_OK) {
-    printf("Cannot configure timer\r\n");
-    goto end;
-  }
-
-  printf("SIMPLY-V FreeRTOS DEMO!\n\r");
-
   // Create FreeRTOS Task
-  BaseType_t mem_1 = xTaskCreate(Task, "t1", configMINIMAL_STACK_SIZE,
-                                 (void *)TASK1_PARAMETER, TASK1_PRIORITY, NULL);
+  BaseType_t res = xTaskCreate(Task, "task", configMINIMAL_STACK_SIZE,
+                                 (void *)TASK_PARAMETER, TASK_PRIORITY, NULL);
 
-  configASSERT(mem_1 == pdPASS);
+  configASSERT(res== pdPASS);
 
   size_t free_heap = xPortGetFreeHeapSize();
 
@@ -116,7 +142,6 @@ int main() {
 
   configASSERT(0); // insufficient RAM->scheduler task returns->vAssertCalled() called
 
-end:
   while (1);
 
   return 0;
