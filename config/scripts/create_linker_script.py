54f98bdd454d6b481ccd68f13a27a6bfbb090f0e
giuseppe <capassog97@gmail.com>
2025-11-06 23:14:46 +0100

    sticking without __main__ entrypoint for now

--- config/scripts/create_linker_script.py
+++ config/scripts/create_linker_script.py
@@ -190,18 +190,17 @@ SECTIONS
     }> {{ initial_memory_name }}
 }
 """
-if __name__ == "__main__":
 
-	template = Template(template_str)
+template = Template(template_str)
 
-	rendered = template.render(
-		current_file_path=os.path.basename(__file__),
-		peripherals=device_dict['peripheral'],
-		memory=device_dict['memory'],
-		global_symbols=device_dict['global_symbols'],
-		initial_memory_name=device_dict['memory'][BOOT_MEMORY_BLOCK]['device'],
-	)
+rendered = template.render(
+	current_file_path=os.path.basename(__file__),
+	peripherals=device_dict['peripheral'],
+	memory=device_dict['memory'],
+	global_symbols=device_dict['global_symbols'],
+	initial_memory_name=device_dict['memory'][BOOT_MEMORY_BLOCK]['device'],
+)
 
-	# === Output to file ===
-	with open(ld_file_name, "w") as f:
-		f.write(rendered)
+# === Output to file ===
+with open(ld_file_name, "w") as f:
+	f.write(rendered)
