3dde7f43229ae31de9aade345d8beec61d159d50
giuseppe <capassog97@gmail.com>
2025-11-10 10:09:17 +0100

    unified HAL with tinyio; simplified application building

---
+++ sw/SoC/lib/uninasoc/inc/stdio.h
@@ -0,0 +1,11 @@
+#ifndef __UNINASOC_STDIO_H
+#define __UNINASOC_STDIO_H
+
+/* 
+ * STDIO API Module. This contains the public API for users.
+ */
+
+int printf(const char *format, ...);
+int scanf(char* format, ...);
+
+#endif // __UNINASOC_STDIO_H
---
+++ sw/SoC/lib/uninasoc/src/uninasoc.c
@@ -0,0 +1,11 @@
+#include "uninasoc.h"
+
+#include "tinyIO.h"
+
+void uninasoc_init()
+{
+    extern const volatile uintptr_t _peripheral_UART_start;
+    // TinyIO init
+    uintptr_t uart_base_address = (uintptr_t)&_peripheral_UART_start;
+    tinyIO_init(uart_base_address);
+}
